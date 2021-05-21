cbbd2e833527c938428cc1e75e2769a321ae5242
abhi9303 <abhinavkumar9315@gmail.com>
2021-05-21 18:26:12 +0530

    AddPatinetDesign

---
+++ app/src/main/java/bih/in/drugmonitor/ui/AddPatient_Activity.java
@@ -0,0 +1,16 @@
+package bih.in.drugmonitor.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bih.in.drugmonitor.R;
+
+public class AddPatient_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_patient_);
+    }
+}
